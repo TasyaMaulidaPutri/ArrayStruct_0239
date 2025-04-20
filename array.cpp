b5e62a8f61fc6b9e4adc719d0f8bf10a73524e73
TasyaMaulidaPutri <tasyamaulidaputri06@gamil.com>
2025-04-20 15:23:14 +0700

    add output

--- array.cpp
+++ array.cpp
@@ -22,6 +22,15 @@ int main()
         cout << "Masukkan nama ke " << i + 1 << " = ";
         cin >> nama[i];
     }
+
+    cout << endl;
+    cout << "Menampilkan Array Nama" << endl;
+    cout << endl;
+
+    for (int i = 0; i < 4; i++)
+    {
+        cout << "Nama ke " << i + 1 << " = " << nama[i] << endl;
+    }
 }
     
         
