7187b2d5cbbe28690f7e7de4ec1da90018903980
TasyaMaulidaPutri <tasyamaulidaputri06@gamil.com>
2025-04-20 15:56:23 +0700

    add deklarasi array dan loop for

--- nestedStruct.cpp
+++ nestedStruct.cpp
@@ -6,3 +6,29 @@ struct DetailAlamat
     string kota;
     string provinsi;
 };
+
+struct Mahasiswa 
+{
+    string nama;
+    string nim;
+    DetailAlamat alamat;
+};
+
+int main()
+{
+    // membuat object dalam array
+    Mahasiswa mhs[2];
+    for (int i = 0; i < 2; i++)
+    {
+        cout << "Mahaiswa ke-" << i + 1 << endl;
+        cout << "masukkan nim = "; 
+        cin >> mhs[i].nim;
+        cout << "Masukkan nama = ";
+        cin >> mhs[i].nama;
+        cout << "Masukkan kota = ";
+        cin >> mhs[i].alamat.kota;
+        cout << "Masukkan provinsi = ";
+        cin >> mhs[i].alamat.provinsi;
+    }
+    
+}
