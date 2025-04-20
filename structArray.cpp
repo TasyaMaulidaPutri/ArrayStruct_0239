072d480c3ed89dfe509b2ff99d58addc7583725c
TasyaMaulidaPutri <tasyamaulidaputri06@gamil.com>
2025-04-20 15:41:51 +0700

    membuat struct dalam array

---
+++ structArray.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+
+struct Mahasiswa
+{
+    string nama;
+    string nim;
+    string alamat;
+};
+
+int main()
+{
+    // membuat object struct dalam array
+    Mahasiswa mhs[4];
+    for (int i = 0; i< 4; i++)
+    {
+        cout << "Mahaiswa ke-" << i + 1 << endl;
+        cout << "masukkan nim = "; 
+        cin >> mhs[i].nim;
+        cout << "Masukkan nama = ";
+        cin >> mhs[i].nama;
+        cout << "Masukkan alamat = ";
+        cin >> mhs[i].alamat;
+    }
+}
